a2092cb9808ac1b1da57e05ffddb1168401a3b2c
DavidSanchezCode <davidalejandrosanchezrincon@gmail.com>
2022-04-09 12:39:18 -0500

    ferreteria3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.variables.productos import src
+import uvicorn
 
 app = FastAPI(
     title="FERRETERIA LOS AMIGOS",
@@ -11,3 +12,9 @@ app.include_router(src)
 @app.get("/")
 async def main_route():
     return [{"name": "david"}]
+
+log_config = uvicorn.config.LOGGING_CONFIG
+log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5000, log_config=log_config)
