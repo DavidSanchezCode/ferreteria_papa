070364c8a5cda4740c24cebf8fffdefca12222b0
DavidSanchezCode <davidalejandrosanchezrincon@gmail.com>
2022-09-02 18:23:36 -0500

    cambio1

--- drives/DBconnection.py
+++ drives/DBconnection.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, MetaData
 
 # ENv variables
-from env.credentials import DB_LENG, DB_DRIV, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from credential.credentials import DB_LENG, DB_DRIV, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 # Defined connection string
 connectionString = DB_LENG + "+" + DB_DRIV + "://" + DB_USER + \
